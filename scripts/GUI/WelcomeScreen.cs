3bc61847e33a01a5953d23fda749b31c047d8caa
Sander <sandermoolin@gmail.com>
2014-05-29 12:34:33 -0400

    Adding parameter adjustment window

--- scripts/GUI/WelcomeScreen.cs
+++ scripts/GUI/WelcomeScreen.cs
@@ -17,6 +17,7 @@ public class WelcomeScreen : MonoBehaviour {
 		// if they're good, create the relevant User Data and a reference to it
 		UserData ud = new UserData(_userNumber, _userAge, _userGender);
 		UserDataMaster.USER_DICT.Add (ud.UserNumber, ud);	
+		Application.LoadLevel("test1");
 		return true;
 	}
 
@@ -30,62 +31,66 @@ public class WelcomeScreen : MonoBehaviour {
 
 	private void OnGUI ()
 	{
-		GUI.skin = skin;
-
-		// box dimensions
-		float width = Screen.width * .5f;
-		float height = Screen.height * .5f;
-
-		GUILayout.BeginArea (new Rect (Screen.width / 2 - width / 2, Screen.height / 2 - height / 2, width, height));
-
-		// WELCOME & INSTRUCTIONS
-		// =======================
-
-		// welcome box
-		GUILayout.Box ("Welcome!");
-
-		// instructions
-		GUILayout.Box ("Instructions go here.");
-
-		// USER INPUT FIELDS
-		// ======================
-
-		// number
-		GUILayout.BeginHorizontal();
-		GUILayout.Box ("Participant number:");
-		_userNumber = GUILayout.TextField (_userNumber);
-		GUILayout.EndHorizontal();
-
-		// age
-		GUILayout.BeginHorizontal();
-		GUILayout.Box ("Your age:");
-		_userAge = GUILayout.TextField (_userAge);
-		GUILayout.EndHorizontal();
-
-		// gender
-		GUILayout.BeginHorizontal();
-		GUILayout.Box ("Your gender:");
-		_userGender = GUILayout.TextField (_userGender);
-		GUILayout.EndHorizontal();
-
-		// SUBMIT
-		// =====================
-		if (GUILayout.Button ("Submit"))
+		if (!AdjustParameters.showWindow)
 		{
-			if (!SubmitInformation())
+			GUI.skin = skin;
+
+			// box dimensions
+			float width = Screen.width * .5f;
+			float height = Screen.height * .5f;
+
+			GUILayout.BeginArea (new Rect (Screen.width / 2 - width / 2, Screen.height / 2 - height / 2, width, height));
+
+			// WELCOME & INSTRUCTIONS
+			// =======================
+
+			// welcome box
+			GUILayout.Box ("Welcome!");
+
+			// instructions
+			GUILayout.Box ("Instructions go here.");
+
+			// USER INPUT FIELDS
+			// ======================
+
+			// number
+			GUILayout.BeginHorizontal();
+			GUILayout.Box ("Participant number:");
+			_userNumber = GUILayout.TextField (_userNumber);
+			GUILayout.EndHorizontal();
+
+			// age
+			GUILayout.BeginHorizontal();
+			GUILayout.Box ("Your age:");
+			_userAge = GUILayout.TextField (_userAge);
+			GUILayout.EndHorizontal();
+
+			// gender
+			GUILayout.BeginHorizontal();
+			GUILayout.Box ("Your gender:");
+			_userGender = GUILayout.TextField (_userGender);
+			GUILayout.EndHorizontal();
+
+			// SUBMIT
+			// =====================
+			if (GUILayout.Button ("Submit"))
 			{
-				error = true;
+				if (!SubmitInformation())
+				{
+					error = true;
+				}
+
+
 			}
 
-		}
+			// error box
+			if (error)
+			{
+				GUILayout.Box ("Could not submit data! Please make sure the \"User Number\" and \"Age\" fields are numbers only.");
+			}
 
-		// error box
-		if (error)
-		{
-			GUILayout.Box ("Could not submit data! Please make sure the \"User Number\" and \"Age\" fields are numbers only.");
+			GUILayout.EndArea();
 		}
-
-		GUILayout.EndArea();
 
 
 	}
---
+++ scripts/GUI/backstage/AdjustParameters.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustParameters : MonoBehaviour {
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Home))
+		{
+			showWindow = !showWindow;
+		}
+	}
+
+	public static int ObjectsToAppear = 5;
+
+	public static bool showWindow = false;
+	private void OnGUI ()
+	{
+		if (showWindow)
+		{
+			float width = Screen.width / 2;
+			float height = Screen.height / 2;
+			GUILayout.BeginArea (new Rect (width - width / 2,
+			                               height - height / 2,
+			                               width,
+			                               height));
+
+			// adjust number of objects to appear
+			GUILayout.Box ("Objects To Appear");
+			GUILayout.BeginHorizontal();
+			ObjectsToAppear = (int)(GUILayout.HorizontalSlider (ObjectsToAppear, 1, 20));
+			GUILayout.Box (ObjectsToAppear.ToString());
+			GUILayout.EndHorizontal();
+
+			// adjust where on screen they can appear
+
+
+
+
+
+
+			GUILayout.EndArea ();
+
+		}
+	}
+}
